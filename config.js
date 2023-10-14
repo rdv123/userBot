8cae79854a6b6a2a18e7f03717711f677418fe78
Denis Rozhkov <rojkoff.d@gmail.com>
2023-10-14 23:17:14 +0300

    ddddd

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
 
-const config = {
-  telegram_credentials: {
-    apiId: 28710354,
-    apiHash: "7ecce441071dd053e51a7dfa70df13d3",
-  },
-  chatIdSubsribe: "-1001674654780",
-  chatIdToSend: "-4069420166"
-};
-module.exports = config;
